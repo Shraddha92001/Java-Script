2bdcc64666a2a5ff01083043fcec38d0be4022e6
shraddha-singh <shraddhasingh92001@gmail.com>
2024-01-06 22:10:30 +0530

    variables

---
+++ 01_variables.js
@@ -0,0 +1,12 @@
+const accountID = 1525
+let accountEmail = " shree@gmal.com" // only use let and const
+var accountPass = "15236" // don't use VAR for the scope
+accountCity = "varanasi"
+
+//accountID = 3 // not allowed
+console.log(accountID);
+accountEmail = "shree112@gmail.com"
+ccountPass = "111116"
+accountCity = "lko"
+
+console.table([accountID, accountEmail, accountPass, accountCity])
