8a776c13a3f231bc984d957f079a82adaa39b164
shraddha-singh <shraddhasingh92001@gmail.com>
2024-01-11 21:44:10 +0530

    confusing conversion

---
+++ conversion_confusing.js
@@ -0,0 +1,22 @@
+//let value = 3;
+//let negvalue = -value
+
+
+let str1 = "hello ";
+let str2 = "shree";
+
+let str3 = str1 + str2;
+console.log(str3);
+
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log(1 + 1);
+console.log("1" + "2" + "2");
+console.log(1 + "2" + "3");
+console.log("1" + 2 + 6);
+console.log((3 + 4) * 5 % 3);
+
+//some tricky conversions
+console.log(+true)
+console.log(+"")
