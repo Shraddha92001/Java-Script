e5c651178a56bb2c3cdd2a182b9379dd01c894f7
shraddha-singh <shraddhasingh92001@gmail.com>
2024-01-11 21:04:04 +0530

    data type

---
+++ data_types.js
@@ -0,0 +1,21 @@
+//Tc39 MDN for JS
+"use strict"; // treat all js code as newer version
+
+//alart(3 + 3) we are using nodeJS, not browser
+console.log(3 + 3)
+
+let name = "shraddha"
+let age = "22"
+let isLoggedin = false
+
+//number => 2 to 53
+//bigint = for very big values
+//string = ""
+//boolean = true/ false
+//null = standalone value
+// undefined = value not defined
+// symbol = unique values
+
+// ****Object****
+console.log(typeof "shree")
+console.log(typeof NULL)
