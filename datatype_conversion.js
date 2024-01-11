19d77c7114758f659a42ed019b0f4b8816c9aa3a
shraddha-singh <shraddhasingh92001@gmail.com>
2024-01-11 21:19:34 +0530

    conversion of data types

---
+++ datatype_conversion.js
@@ -0,0 +1,11 @@
+let score = "ANDBDB"
+let bool = true
+console.log(typeof score)
+
+let valueInNumber = Number(score) // converting sting into number by using Number function
+console.log(typeof(valueInNumber))
+console.log(valueInNumber)
+
+let BoolInNumber = String(bool) // converting boolean into String by using Sting function
+console.log(typeof(BoolInNumber))
+console.log(BoolInNumber)
