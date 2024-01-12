a79666ef626b1a38102dd245d865b1e7922e40fb
shraddha-singh <shraddhasingh92001@gmail.com>
2024-01-12 14:47:21 +0530

    datatype summery

---
+++ datatype_summery.js
@@ -0,0 +1,17 @@
+// type of datatype is primitive and non-primitive
+
+//primitive 
+// 7 type == string, null , number, symbol, undefined , boolean , bigint
+
+const score = 100
+const scorevalue = 100.11
+const isLogIn = false
+const OutSidetemp = null
+
+let useName;
+
+const id = Symbol('1252')
+const otherID = Symbol('1252')
+console.log(id === otherID);
+//non - primitive :: (reference type)
+// 3 type == array, object,  function
