01df4287e18cc55499a3845e258eb08279e7427c
shraddha-singh <shraddhasingh92001@gmail.com>
2024-01-12 15:13:17 +0530

    summery

--- datatype_summery.js
+++ datatype_summery.js
@@ -13,5 +13,26 @@ let useName;
 const id = Symbol('1252')
 const otherID = Symbol('1252')
 console.log(id === otherID);
+
+const BigInt = 145698712354545
+console.log(typeof BigInt);
+
+
 //non - primitive :: (reference type)
 // 3 type == array, object,  function
+
+//Array
+const cars = ["BMW", "audi", "maruti"]
+console.log(cars);
+
+//object
+let myOB = {
+    name: "shreee",
+    age: 22,
+}
+
+//function
+const myfunction = function() {
+    console.log("hello shree");
+}
+console.log(typeof myfunction);
