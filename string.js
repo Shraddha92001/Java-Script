b8b7cc803056f0da0d077d767f6338c9a11aab04
shraddha-singh <shraddhasingh92001@gmail.com>
2024-01-13 22:03:18 +0530

    string basic

---
+++ string.js
@@ -0,0 +1,11 @@
+console.log("hello");
+console.log('world');
+const name = "shree"
+const num = 101
+    // console.log(name + num + "value"); not a good way to concatenat
+
+console.log(`hello my name is ${name} and my number is ${num}.`); //by using backtick we can concatenat easily
+
+// declaring string
+const myname = new String("shree")
+console.log(myname);
