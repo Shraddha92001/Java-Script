bc3535010f46588cad4751eabe4d9def2dae0ba4
shraddha-singh <shraddhasingh92001@gmail.com>
2024-01-14 22:39:40 +0530

    number and maths

--- num_math.js
+++ num_math.js
@@ -5,3 +5,13 @@ const myfloat = 6022.11
 
 
 console.log(myint);
+
+
+const balance = new Number(10000)
+console.log(balance);
+console.log(balance.toString().length); // find the leanth of string
+console.log(balance.toFixed(2)); // priority after decimal
+
+const newNumber = 1000.22
+console.log(newNumber);
+console.log(newNumber.toPrecision(6));
