6676166590bb897ad489da00450c92bf3899c161
shraddha-singh <shraddhasingh92001@gmail.com>
2024-01-15 22:19:01 +0530

    date object

---
+++ date_objects.js
@@ -0,0 +1,7 @@
+const d = new Date();
+const date = new Date("October 13, 2014 11:13:00");
+const d1 = new Date(2018, 11, 24, 10, 33, 30, 0);
+
+console.log(d);
+console.log(d1);
+console.log(date);
