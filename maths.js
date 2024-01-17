be144a5e78d912dd17fa6c2b44b51271fef96b2b
shraddha-singh <shraddhasingh92001@gmail.com>
2024-01-17 12:46:26 +0530

    Math function

---
+++ maths.js
@@ -0,0 +1,20 @@
+console.log(Math);
+//abs()
+console.log(Math.abs(-4)); //negative convert into positive
+//round()
+console.log(Math.round(5.6));
+//Max
+console.log(Math.max(4, 2, 52, 69));
+//Min
+console.log(Math.min(4, 2, 52, 69));
+//floor()
+console.log(Math.floor(4.9));
+//ceil()
+console.log(Math.ceil(4.2));
+
+//random
+console.log(Math.random());
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
