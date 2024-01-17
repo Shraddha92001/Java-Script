79dc661381541b55d66103db3485352faed48d44
shraddha-singh <shraddhasingh92001@gmail.com>
2024-01-17 13:24:24 +0530

    array in jS

---
+++ Array.js
@@ -0,0 +1,15 @@
+const Myarray = [1, 22, 25, 26, 91, 24, 13, 15, 44, 12]
+
+
+//********************OPRATIONS***************** 
+//push
+Myarray.push(4)
+Myarray.push(17)
+Myarray.pop()
+
+Myarray.unshift(100) // add value to the front of the array
+
+Myarray.shift() //Removes the first element from an array and returns it.
+console.log(Myarray);
+
+console.log();
