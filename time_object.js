3ce41057fb0d5aca1275ec3611182d2ce8d2fba5
shraddha-singh <shraddhasingh92001@gmail.com>
2024-01-17 13:08:35 +0530

    time objects

---
+++ time_object.js
@@ -0,0 +1,17 @@
+let mydate = new Date("01-12-2023")
+
+let mytime = Date.now()
+console.log(mytime);
+console.log(mydate.getTime);
+console.log(Math.floor(Date.now() / 1000));
+
+
+//* ** ** ** ** ** ** ** ** ** ** methods ** ** ** ** ** ** *
+let newdate = new Date()
+
+console.log(newdate);
+console.log(newdate.getMonth() + 1);
+console.log(newdate.getday);
+console.log(newdate);
+console.log(newdate);
+console.log(newdate);
