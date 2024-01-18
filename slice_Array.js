4a3e40a301c53fd44676df0d97c590e28438885c
shraddha-singh <shraddhasingh92001@gmail.com>
2024-01-18 21:06:39 +0530

    slice and splice in array

---
+++ slice_Array.js
@@ -0,0 +1,12 @@
+// slice and Splice
+
+const Myarray = [1, 25, 26, 4, 5, 3, 6, 8]
+console.log("A", Myarray);
+
+const myn1 = Myarray.slice(1, 3)
+console.log(myn1);
+
+console.log("B", Myarray);
+
+const myn2 = Myarray.splice(1, 3)
+console.log(myn2);
