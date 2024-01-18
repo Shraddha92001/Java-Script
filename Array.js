fd81ae1f4867e072f38e8d803feab98f83f0d2f8
shraddha-singh <shraddhasingh92001@gmail.com>
2024-01-18 20:52:28 +0530

    array

--- Array.js
+++ Array.js
@@ -1,6 +1,11 @@
+//array 
 const Myarray = [1, 22, 25, 26, 91, 24, 13, 15, 44, 12]
 
+const myfrend = ["neha", "rahul", "priya"]
+console.log(myfrend);
 
+const arrau2 = new Array(1, 2, 5, 6, 4, 5, 6)
+console.log(arrau2[5]);
 //********************OPRATIONS***************** 
 //push
 Myarray.push(4)
