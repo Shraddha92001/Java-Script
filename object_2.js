cdef3c00977467a90553e0c4485bb9b4f68ef45a
shraddha-singh <shraddhasingh92001@gmail.com>
2024-01-22 21:21:46 +0530

    declaration  object by singleton = constructor

---
+++ object_2.js
@@ -0,0 +1,10 @@
+//declaration  object by singleton = constructor 
+
+const tideruser = new Object() //singleton object
+console.log(tideruser);
+
+const tinderuser2 = {} // non singleton object
+tinderuser2.id = "1253aba"
+tinderuser2.name = "shreee"
+tinderuser2.email = "shree@gmail.com"
+console.log(tinderuser2);
