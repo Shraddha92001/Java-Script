5fbd16a365f2ba248d8c9084ef330dba81f2c4ec
shraddha-singh <shraddhasingh92001@gmail.com>
2024-01-24 15:03:48 +0530

    using Array in Function

---
+++ function_array.js
@@ -0,0 +1,9 @@
+//using Array in Function
+
+const Array = [2000, 555, 100, 120]
+
+function getArray(myArray) { //... is rest and spread  ---- put all element in array
+    return myArray[3]
+}
+console.log(getArray(Array))
+    //console.log(customer(2, 500, 6, 200, 400)) //access multiple all the value we use spread
