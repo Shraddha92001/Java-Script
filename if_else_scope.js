e2aa9b2d3bedc2f9b9ac3d099626f56ad44fc97d
shraddha-singh <shraddhasingh92001@gmail.com>
2024-01-24 21:16:37 +0530

    nested scope in if_else

---
+++ if_else_scope.js
@@ -0,0 +1,11 @@
+// nested scope in if_else 
+
+if (true) {
+    const username = "Shree "
+    if (username === "Shree ") {
+        const website = "youtube"
+        console.log(username + website);
+    }
+    // console.log(website);
+}
+// console.log(username);
