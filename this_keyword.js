0de142546531bf031d77eab62adb143d7171e828
shraddha-singh <shraddhasingh92001@gmail.com>
2024-01-25 21:10:52 +0530

    this keyword

---
+++ this_keyword.js
@@ -0,0 +1,11 @@
+//this keyword user for refer current contest 
+const user = {
+    username: "shree",
+    prive: 500,
+
+    wlcomeMSG: function() {
+        console.log(` ${this.username},welcome to website`);
+    }
+}
+user.wlcomeMSG
+console.log(user);
