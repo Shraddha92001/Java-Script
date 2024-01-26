8e4f4233679d093de819fd38cf17711402518b1b
shraddha-singh <shraddhasingh92001@gmail.com>
2024-01-26 21:20:25 +0530

    else if

---
+++ control flow/else_if.js
@@ -0,0 +1,12 @@
+// eg.. else IF
+const balance = 1000
+
+if (balance < 500) {
+    console.log("less then 500");
+} else if (balance < 750) {
+    console.log("less then 750");
+} else if (balance < 950) {
+    console.log("less then 950");
+} else {
+    console.log("less then 120000");
+}
