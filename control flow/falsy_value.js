f5efa866155b06d489e507b8c362ba079c571bfb
shraddha-singh <shraddhasingh92001@gmail.com>
2024-01-26 21:46:45 +0530

    falsy value

---
+++ control flow/falsy_value.js
@@ -0,0 +1,9 @@
+// falsy value  ------- assuming a false to any value is called falsy value
+// false, 0, -0, "", null , undefined, NaN
+const useremail = "shree@gmail"
+
+if (useremail) {
+    console.log("got user email");
+} else {
+    console.log("not have value");
+}
