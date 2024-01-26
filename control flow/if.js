54b3811e53bc3f1448697d82727a8848b159cad5
shraddha-singh <shraddhasingh92001@gmail.com>
2024-01-26 21:09:08 +0530

    if control flow

---
+++ control flow/if.js
@@ -0,0 +1,10 @@
+//Control_flow = logic flow
+
+// if syntex
+// const LoggedIN = true
+const temprature = 41
+const temprature1 = 41
+if (temprature < 50) {
+    console.log("less then 50");
+}
+// console.log("execute");
