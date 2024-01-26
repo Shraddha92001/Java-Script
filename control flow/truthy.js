6eac2de9c3a6a81e737afb7199074d1e38ec3336
shraddha-singh <shraddhasingh92001@gmail.com>
2024-01-26 21:41:22 +0530

    truthy value

---
+++ control flow/truthy.js
@@ -0,0 +1,9 @@
+// truthy  ------- assuming a true value is called truthy value
+
+const useremail = "shree@gmail"
+
+if (useremail) {
+    console.log("got user email");
+} else {
+    console.log("not have value");
+}
