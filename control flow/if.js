2cf6d25f4ef209196b39a4b050a12ed475e25d3a
shraddha-singh <shraddhasingh92001@gmail.com>
2024-01-26 21:13:11 +0530

    if Control_flow

--- control flow/if.js
+++ control flow/if.js
@@ -8,3 +8,10 @@ if (temprature < 50) {
     console.log("less then 50");
 }
 // console.log("execute");
+const scope = 200
+
+if (scope > 100) {
+    let power = "fly"
+    console.log(`user power:${power}`);
+}
+// console.log(`user power: ${power}`);
