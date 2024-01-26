155c39c2c63c0275dbb18d783147730fb50dfcf4
shraddha-singh <shraddhasingh92001@gmail.com>
2024-01-26 21:36:11 +0530

    switch case

---
+++ control flow/switch.js
@@ -0,0 +1,41 @@
+//for number
+const month = 3
+switch (month) {
+    case 1:
+        console.log("jan");
+        break;
+    case 2:
+        console.log("fab");
+        break;
+    case 3:
+        console.log("march");
+        break;
+    case 4:
+        console.log("apr");
+        break;
+    case 5:
+        console.log("may");
+        break;
+
+    default:
+        console.log(" default case match");
+        break;
+}
+
+//for string
+
+const month1 = "jan"
+switch (month1) {
+    case "jan":
+        console.log("jan ");
+        break;
+    case "fab":
+        console.log("fab");
+        break;
+    case "march":
+        console.log("march1");
+        break;
+    default:
+        console.log(" default case match");
+        break;
+}
