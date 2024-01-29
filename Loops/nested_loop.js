72b6e607562824badfa1ff3c8f1245b4557fb6fe
shraddha-singh <shraddhasingh92001@gmail.com>
2024-01-29 21:45:31 +0530

    nested loop

---
+++ Loops/nested_loop.js
@@ -0,0 +1,12 @@
+ for (let i = 0; i <= 10; i++) {
+     console.log(`outer loop: ${i}`);
+     for (let j = 0; j <= 5; j++) {
+         console.log(`inner loop ${j} and outer loop ${i}`);
+         //      for (let k = 0; k <= 10; k++) {
+         //          console.log(`second inner loop ${k} and outer loop ${i} and inner loop is ${j}`);
+
+         //      }
+         //  
+     }
+
+ }
