a101a05ed052b2758abf921c4a6a850003beb689
shraddha-singh <shraddhasingh92001@gmail.com>
2024-01-30 22:03:14 +0530

    for_of loop

---
+++ Loops/for_of_loop.js
@@ -0,0 +1,12 @@
+// for_of loop
+
+const arr = [1, 2, 5, 3, 6]
+for (const num of arr) {
+    console.log(num);
+}
+
+
+const str = "hello shree"
+for (const num of str) {
+    console.log(`each char is ${str}`)
+}
