524bdc93261447c3683fdd8f98690b4e18318d61
shraddha-singh <shraddhasingh92001@gmail.com>
2024-02-14 00:21:45 +0530

    nested object

---
+++ object/nested_object.js
@@ -0,0 +1,13 @@
+const myobj = {
+    name:"shree",
+    age : 25,
+
+    cars: {
+        car1: "Ford",
+        car2: "BMW",
+        car3: "Fiat"
+    }
+}
+console.log(myobj);
+console.log(myobj.cars);
+console.log(myobj.cars.car1);
