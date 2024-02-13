023293f6bf70bc7d004059d4688be06e8b9a4e63
shraddha-singh <shraddhasingh92001@gmail.com>
2024-02-14 00:32:22 +0530

    delete obj

---
+++ object/deleted_object.js
@@ -0,0 +1,15 @@
+let myobj = {
+    name:"shree",
+    age : 25,
+
+    cars: {
+        car1: "Ford",
+        car2: "BMW",
+        car3: "Fiat"
+    }
+}
+console.log(myobj);
+console.log(myobj.cars);
+console.log(myobj.cars.car1);
+
+myobj = null;
